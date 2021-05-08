fc595a75d98340863d3e5530b1a83d68cf4d05d7
nazy67 <nazykh67@gmail.com>
2021-05-08 00:15:56 -0500

    added outputs.tf, providers.tf

---
+++ qa_env/rds_qa/outputs.tf
@@ -0,0 +1,11 @@
+output "username" {
+  value = module.rds_module.module_username
+}
+
+output "address" {
+  value = module.rds_module.module_address
+}
+
+output "pet_name" {
+  value = module.rds_module.module_pet_name_value
+}
