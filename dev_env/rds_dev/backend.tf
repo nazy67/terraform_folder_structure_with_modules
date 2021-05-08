0222caed5876397484fb92a6d6487282978e148b
nazy67 <nazykh67@gmail.com>
2021-05-07 23:24:39 -0500

    modified main.tf, backend.tf

--- dev_env/rds_dev/backend.tf
+++ dev_env/rds_dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-  bucket = "terraform-nazy-state"
-  key = "dev/rds.tfstate"
-  region = "us-east-1"
-  dynamodb_table = "terraform-state-locks"
-  } 
+    bucket         = "terraform-nazy-state"
+    key            = "dev/rds.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+  }
 }
--- dev_env/rds_dev/main.tf
+++ dev_env/rds_dev/main.tf
@@ -1,8 +1,8 @@
 module "rds_module" {
-  source =  "../../modules/rds"
-  env = "dev"
-  storage = 10
-  skip_snapshot = "true"
+  source         = "../../modules/rds"
+  env            = "dev"
+  storage        = 10
+  skip_snapshot  = "true"
   instance_class = "db.t2.micro"
-  username = "dev_user"
+  username       = "dev_user"
 }
