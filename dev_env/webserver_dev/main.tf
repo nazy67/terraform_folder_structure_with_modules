a46c36a2a0f6eb233ce7ed0581f4b42f6490076d
nazy67 <nazykh67@gmail.com>
2021-05-07 23:17:55 -0500

    modified main.tf

--- dev_env/webserver_dev/main.tf
+++ dev_env/webserver_dev/main.tf
@@ -1,12 +1,19 @@
 module "webserver_module" {
   source = "../../modules/webserver"
-  
-  env = "dev"
+
+  env           = "dev"
   instance_type = "t2.micro"
-  
+
   remote_state = {
-      bucket = "terraform-nazy-state"
-      key = "dev/rds.tfstate"
-      region = "us-east-1"
+    bucket = "terraform-nazy-state"
+    key    = "dev/rds.tfstate"
+    region = "us-east-1"
   }
+}
+resource "random_pet" "test" {
+  count = 1
+}
+
+output "pet_name_value" {
+  value = random_pet.test[*].id
 }
