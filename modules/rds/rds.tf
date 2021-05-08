3ae3ed186eeae00a5aa4d2f9054f592804ac271c
nazy67 <nazykh67@gmail.com>
2021-05-08 00:09:03 -0500

    modified rds.tf, variables.tf

--- modules/rds/rds.tf
+++ modules/rds/rds.tf
@@ -1,14 +1,14 @@
 resource "aws_db_instance" "rds-db" {
-  allocated_storage    = var.storage
-  storage_type         = "gp2"
-  engine               = "mariadb"
-  engine_version       = "10.5"
-  instance_class       = var.instance_class
-  identifier           = "${var.env}-rds"
-  name                 = "my_rds"
-  username             = var.username
-  password             = random_password.password.result
-  skip_final_snapshot = var.skip_snapshot 
+  allocated_storage         = var.storage
+  storage_type              = "gp2"
+  engine                    = "mariadb"
+  engine_version            = "10.5"
+  instance_class            = var.instance_class
+  identifier                = "${var.env}-rds"
+  name                      = "my_rds"
+  username                  = var.username
+  password                  = random_password.password.result
+  skip_final_snapshot       = var.skip_snapshot
   final_snapshot_identifier = var.skip_snapshot == true ? null : "${var.env}-rds-snapshot" # it checks if final snapshot has to be taken or not. this means if it's dev env make a final snapshot , if the other env dont make a snapshot
   vpc_security_group_ids    = [aws_security_group.rds_sg.id]
   apply_immediately         = true # flag to instruct the service to apply the change immediately 
@@ -40,4 +40,4 @@ resource "aws_security_group_rule" "local_laptop" {
   protocol          = "TCP"
   cidr_blocks       = ["0.0.0.0/0"] # your IP address
   security_group_id = aws_security_group.rds_sg.id
-} 
+}
--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,27 +1,27 @@
 # Define root module variables
 
 variable "env" {
-    description = "the name of the environment"
-    type = string
-} 
+  description = "the name of the environment"
+  type        = string
+}
 
 variable "storage" {
-    description = "the storage size of database"
-    type = string
+  description = "the storage size of database"
+  type        = string
 }
 
 variable "skip_snapshot" {
-    description = "it will skip the final snapshot incase if someone deletes database by accident"
-    type = bool
+  description = "it will skip the final snapshot incase if someone deletes database by accident"
+  type        = bool
 }
 
 variable "instance_class" {
-    description = " database instance class"
-    type = string
+  description = " database instance class"
+  type        = string
 }
 
 
 variable "username" {
-    description = "the username of database"
-    type = string
+  description = "the username of database"
+  type        = string
 }
