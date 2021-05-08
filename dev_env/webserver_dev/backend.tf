1585a4ee00580fb5387cc8809c376cc0ee6273c1
nazy67 <nazykh67@gmail.com>
2021-05-07 19:12:54 -0500

    added dev_env

---
+++ dev_env/webserver_dev/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-nazy-state"
+    key    = "dev/webserver.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "terraform-state-locks"
+  }
+}
