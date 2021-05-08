ce0343b4864baef3490a9022ed94b2a297447dd6
nazy67 <nazykh67@gmail.com>
2021-05-08 00:08:30 -0500

    modified output.tf, random.tf

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -1,13 +1,13 @@
 output "module_address" { # rds endpoint   
-    value = aws_db_instance.rds-db.address
-    description = "this is the  address of rds instance"
+  value       = aws_db_instance.rds-db.address
+  description = "this is the  address of rds instance"
 }
 output "module_username" {
-    value = aws_db_instance.rds-db.username
-    description = "this is the  address of rds instance"
+  value       = aws_db_instance.rds-db.username
+  description = "this is the  address of rds instance"
 }
 output "password" {
-    value = aws_db_instance.rds-db.password
-    description = "this is the  address of rds instance"
-    sensitive = true 
+  value       = aws_db_instance.rds-db.password
+  description = "this is the  address of rds instance"
+  sensitive   = true
 }
--- modules/rds/random_passwd.tf
+++ modules/rds/random_passwd.tf
@@ -1,5 +1,5 @@
 resource "random_password" "password" {
-  length = 16
-  special = true
+  length           = 16
+  special          = true
   override_special = "_%"
 }
