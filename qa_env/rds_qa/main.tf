94021e71b4eef4747b484dd569292fb0aafc4808
nazy67 <nazykh67@gmail.com>
2021-05-08 00:16:56 -0500

    modified main.tf, backend.tf

--- qa_env/rds_qa/main.tf
+++ qa_env/rds_qa/main.tf
@@ -1,15 +1,8 @@
 module "rds_module" {
-  source =  "../../modules/rds"
-  env = "qa"
-  storage = 15
-  skip_snapshot = "true"
+  source         = "../../modules/rds"
+  env            = "qa"
+  storage        = 15
+  skip_snapshot  = "true"
   instance_class = "db.t2.micro"
-  username = "qa_user"
-}
-output "username" {
-  value = module.rds_module.module_username
-}
-
-output "address" {
-  value = module.rds_module.module_address
+  username       = "qa_user"
 }
