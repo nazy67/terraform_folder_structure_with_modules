30ca0cc77fad91583c01e8fc0792aadfa36f6ac9
nazy67 <nazykh67@gmail.com>
2021-05-07 20:36:30 -0500

    modified remote_state.tf

--- modules/webserver/remote_state.tf
+++ modules/webserver/remote_state.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "rds" {
   backend = "s3"
   config  = {
     bucket = var.remote_state["bucket"]
-    key    = var.remote_state["key"]        #"${var.remote_state["workspace_key_prefix"]}/${var.env}/${var.remote_state["key"]}" 
+    key    = var.remote_state["key"] #"${var.remote_state["workspace_key_prefix"]}/${var.env}/${var.remote_state["key"]}" 
     region = var.remote_state["region"]
    }                                                                      
   }
